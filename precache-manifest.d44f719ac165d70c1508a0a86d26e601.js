f84b6dc751c509206e7557ad7479a909f7c747de
James Perona <j@mesperona.com>
2020-01-11 16:47:44 -0600

    Updates

---
+++ precache-manifest.d44f719ac165d70c1508a0a86d26e601.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c9bd1a76549c68392308d27b0e69494",
+    "url": "/sba_scoreboard/index.html"
+  },
+  {
+    "revision": "077caa843b1fcce49228",
+    "url": "/sba_scoreboard/static/css/main.5bea1756.chunk.css"
+  },
+  {
+    "revision": "1b8ef052182b69190787",
+    "url": "/sba_scoreboard/static/js/2.6ef25719.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/sba_scoreboard/static/js/2.6ef25719.chunk.js.LICENSE"
+  },
+  {
+    "revision": "077caa843b1fcce49228",
+    "url": "/sba_scoreboard/static/js/main.e8b70b59.chunk.js"
+  },
+  {
+    "revision": "93d49dcd62fadc4f9f19",
+    "url": "/sba_scoreboard/static/js/runtime-main.d666c985.js"
+  }
+]);
