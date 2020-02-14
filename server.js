67c5ca3c1d1d1ce1825a6ef467d006b12c7b0167
James Perona <j@mesperona.com>
2020-02-13 18:00:50 -0600

    added putnam county girls

--- server.js
+++ server.js
@@ -8,18 +8,26 @@ var CONTACTS_COLLECTION = "contacts";
 var app = express();
 app.use(bodyParser.json());
 
+// console.log(process.env);
+
+var client = mongodb.MongoClient("mongodb://jim:hardpassword1@ds139334.mlab.com:39334/heroku_glcps0qt");
+var dbName = "heroku_glcps0qt";
+
 // Create a database variable outside of the database connection callback to reuse the connection pool in your app.
 var db;
-
+// "mongodb+srv://scoreboard:U0ecgZGgNXcPRQSc@cluster0-zlyky.mongodb.net/test?retryWrites=true&w=majority"
+//bxsg6wmNFW@KsUp
 // Connect to the database before starting the application server.
-mongodb.MongoClient.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/test", function (err, client) {
+client.connect(function (err, clyent) {
   if (err) {
     console.log(err);
     process.exit(1);
   }
 
+  console.log('reach here');
+
   // Save database object from the callback for reuse.
-  db = client.db({useUnifiedTopology: true});
+  db = clyent.db(dbName); //, {useUnifiedTopology: true}
   console.log("Database connection ready");
 
   // Initialize the app.
@@ -42,13 +50,13 @@ function handleError(res, reason, message, code) {
 */
 
 app.get("/api/rosters", function(req, res) {
-    db.collection(rosters).find({}).toArray(function(err, docs) {
-        if (err) {
-          handleError(res, err.message, "Failed to get rosters.");
-        } else {
-          res.status(200).json(docs);
-        }
-    });
+    // db.collection(rosters).find({}).toArray(function(err, docs) {
+    //     if (err) {
+    //       handleError(res, err.message, "Failed to get rosters.");
+    //     } else {
+    //       res.status(200).json(docs);
+    //     }
+    // });
 });
 
 app.post("/api/rosters", function(req, res) {
